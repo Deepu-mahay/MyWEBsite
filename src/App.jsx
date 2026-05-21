4312efb3662ff2c5edf79e99dff25b372e98b0e2
Dpinder Mahay <dpinder.mahay.786@gmail.com>
2026-05-21 22:58:33 +0530

    first commit Navba

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Navbar from "@/layout/Navbar"
+import Hero from "@/sections/Hero"
+import About from "@/sections/About"
+import Projects from "@/sections/Projects"
+import Testimonials from "@/sections/Testimonials"
+import Experience from "@/sections/Experience"
+import Contact from "@/sections/Contact"
+
+
+function App() {
+
+
+  return (
+    <div className="min-h-screen overflow-x-hidden">
+      <Navbar />
+      <main>
+        <Hero/>
+        <About />
+        <Projects />
+        <Testimonials />
+        <Experience />
+        <Contact />
+      </main>
+    </div>
+  )
+}
+
+export default App
